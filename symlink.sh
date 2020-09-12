672c8d26c32370085eedb85eaee1de39cc9fbc34
ling1729 <renlingfeng21@gmail.com>
2020-09-12 16:19:44 -0700

    make script better

--- symlink.sh
+++ symlink.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# Sets the destination path, user's home by default
 if [ "$1" ]
 then
-	destPath=$1
+  destPath=$1
 else
-	destPath="$HOME"
+  destPath="$HOME"
 fi
 
-# Exclude .config folder to be symliked seperately and other files
-exclude=( ".config" ".git" ".gitignore" "symlink.sh")
+# Exclude .config folder to be symliked seperately and ignore other files
+exclude=(".config" ".git" ".gitignore" "symlink.sh")
 
-# Math hidden files with globbing
-shopt -s dotglob 
+# Match hidden files with globbing
+shopt -s dotglob
 
-# Where dotfiles are to be symliked to	
-echo "${destPath}" 
+# Shows where dotfiles are to be symliked to
+echo "${destPath}"
 
 # The directory the folder is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
@@ -24,18 +25,21 @@ for file in *; do
     for (( index = 0; index < ${#exclude[@]}; index++ )); do
         if [[ ${file} != ${exclude[${index}]} ]]; then
             ln -sfn ${DIR}/${file} ${destPath}/${file}
-        fi  
+        fi
     done
 done
 
 mkdir -p ${destPath}/.config # Create a .config folder if it doesn't already exist
 
+# Files to be excluded from .config
+exclude2=()
+
 # Symlink files in .config directory
 for file in .config/*; do
-    for (( index = 0; index < ${#exclude[@]}; index++ )); do
-        if [[ ${file} != ${exclude[${index}]} ]]; then
+    for (( index = 0; index < ${#exclude2[@]}; index++ )); do
+        if [[ ${file} != ${exclude2[${index}]} ]]; then
             ln -sfn ${DIR}/${file} ${destPath}/${file}
-        fi  
+        fi
     done
 done
 
